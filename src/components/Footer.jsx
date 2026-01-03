f5faf1def4ad0abbf009edf8c9a1b9dd132eac73
J0ker11 <primetech444@gmail.com>
2026-01-03 23:19:09 +0100

    fix: fixed social handle

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import Container from "./Container";
 import Button from "./ui/Button";
+import Instagram from "../assets/icons/Instagram";
+import Twitter from "../assets/icons/Twitter";
+import Tiktok from "../assets/icons/Tiktok";
+import Youtube from "../assets/icons/Youtube";
 
 const Footer = () => {
   const footerSection = [
@@ -33,7 +37,8 @@ const Footer = () => {
 
   return (
     <section className="w-full text-black satoshi">
-      <Container className="px-3 flex justify-between ">
+      <Container className="px-3">
+        <div className="flex justify-between">
         <div className="grid grid-cols-2 lg:grid-cols-3  gap-10 last:gap-8 md:gap-[72px] ">
         {footerSection.map((item) => (
           <div key={item.id}>
@@ -68,6 +73,7 @@ const Footer = () => {
           </div>
         ))}
         </div>
+        
             {/* Button */}
             <div className="hidden md:block">
                 <h2 className="font-bold text-lg lg:text-xl mb-4">Sign up for our newsletter</h2>
@@ -75,6 +81,18 @@ const Footer = () => {
                 <input type="text" placeholder="Enter Email" className="px-6 border min-w-[250px] lg:min-w-[498px] border-[#949494] outline-[#949494] py-[14px] rounded-full " />
                 <Button className=" absolute right-[6px]  px-6 py-2 inline-flex justify-center items-center text-white font-medium text-lg bg-[#563ACC] rounded-full">Subscribe</Button>
             </div>
+            </div>
+            </div>
+
+            {/* Socials */}
+            <div className="flex flex-col md:flex-row gap-6 justify-between mt-10 md:mt-[54px] ">
+                <div className="flex gap-10">
+                    <Instagram />
+                    <Twitter />
+                    <Tiktok />
+                    <Youtube />
+                </div>
+                <p className="font-normal text-sm">&copy;2023 FinanceFast All Rights Reserved.</p>
             </div>
       </Container>
     </section>
