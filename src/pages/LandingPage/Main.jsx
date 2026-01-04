d4236e02ad0223a899a0c7cdafca72f152d56e90
J0ker11 <primetech444@gmail.com>
2026-01-04 23:58:59 +0100

    feat: Added main  section

---
+++ src/pages/LandingPage/Main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Container from '../../components/Container'
+
+const Main = () => {
+  return (
+    <section className='w-full bg-white satochi'>
+        <Container className=''>
+            <div>
+                <h2 className='text-[6364FF'>
+                Cross-Border Transactions
+                </h2>
+            </div>
+        </Container>
+    </section>
+  )
+}
+
+export default Main
