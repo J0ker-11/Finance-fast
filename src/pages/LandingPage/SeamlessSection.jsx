6dd491c88338a21f120f3b2b9d6cbd8c8c759d71
J0ker11 <primetech444@gmail.com>
2026-01-07 22:00:47 +0100

    Feat: Added transaction section

---
+++ src/pages/LandingPage/SeamlessSection.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Container from '../../components/Container'
+
+const SeamlessSection = () => {
+  return (
+    <section>
+        <Container>
+            
+        </Container>
+    </section>
+  )
+}
+
+export default SeamlessSection
